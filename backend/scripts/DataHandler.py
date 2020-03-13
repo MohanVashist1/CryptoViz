1564289f96389086e3f73508295873ad59f41480
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-03-12 22:54:15 -0400

    Moved scraper to DataHandler

--- backend/scripts/DataHandler.py
+++ backend/scripts/DataHandler.py
@@ -14,6 +14,7 @@ import requests
 from binance.client import Client
 from dateutil import parser
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
 
 class BinanceWrapper:
@@ -101,6 +102,49 @@ class BinanceWrapper:
             self.getCryptoDataBinance(
                 crypto, kline_size, save=True)
 
+class _Scraper:
+    def scrape(self, url, html_id):
+        top_10 = []
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        for ele in soup.find(id=html_id).find('tbody').find_all('tr')[:10]:
+            tmp = {}
+            info = ele.find_all('td')
+            tmp['Rank'] = int(info[0].find('a').text)
+            tmp['Symbol'] = info[1].find('a').text
+            tmp['Market Cap'] = self.__normalize_val(info[2].find('a').text)
+            tmp['Price'] = self.__normalize_val(info[3].find('a').text)
+            tmp['Volume'] = self.__normalize_val(info[4].find('a').text)
+            top_10.append(tmp)
+        return top_10
+
+    def __normalize_val(self, val):
+        val = val[1:].replace(',','')
+        if val[-1] == "K":
+                val = float(val[:-1]) * 1000
+        elif val[-1] == "M":
+            val = float(val[:-1]) * 1000000
+        elif val[-1] == "B":
+            val = float(val[:-1]) * 1000000000
+        else:
+            val = float(val)
+        return val
+
+def retrieve_top_gainers_hourly():
+    sc = _Scraper()
+    return sc.scrape('https://bitscreener.com/screener/gainers-losers?tf=1h#gainers', 'gainers')
+
+def retrieve_top_losers_hourly():
+    sc = _Scraper()
+    return sc.scrape('https://bitscreener.com/screener/gainers-losers?tf=1h#gainers', 'losers')
+
+def retrieve_top_gainers_daily():
+    sc = _Scraper()
+    return sc.scrape('https://bitscreener.com/screener/gainers-losers?tf=24h#gainers', 'gainers')
+
+def retrieve_top_losers_daily():
+    sc = _Scraper()
+    return sc.scrape('https://bitscreener.com/screener/gainers-losers?tf=24h#gainers', 'losers')
 
 if __name__ == "__main__":
     # BinanceWrapper().getAllCryptoDataBinance("1m", save=True)
@@ -116,4 +160,8 @@ if __name__ == "__main__":
     # #     }
     # # )
     # # print(response)
+    # print(retrieve_top_gainers_hourly())
+    # print(retrieve_top_losers_hourly())
+    # print(retrieve_top_losers_daily())
+    # print(retrieve_top_gainers_daily())
     pass
