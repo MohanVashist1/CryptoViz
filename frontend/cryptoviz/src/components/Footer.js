46adcd891ae517ba63922720c70ab9de714a8d62
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-03-12 21:14:10 -0400

    Modified styling organization a bit.

--- frontend/cryptoviz/src/components/Footer.js
+++ frontend/cryptoviz/src/components/Footer.js
@@ -2,11 +2,12 @@ import "bootswatch/dist/lux/bootstrap.min.css";
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style/footer.css";
+import "../style/main.css";
 
 function Footer() {
   return (
     <footer>
-      <div className = "credits-link">
+      <div className = "uppercase">
         <Link to="/credits">Credits</Link>
       </div>
     </footer>
