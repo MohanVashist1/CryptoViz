907766b4341c35d40e9b6f66a4c46e04d7243170
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-03-14 11:56:07 -0400

    Fixed process termination bug in Scheduler.

--- backend/scripts/Scheduler.py
+++ backend/scripts/Scheduler.py
@@ -61,10 +61,10 @@ def schedule_tasks():
         process.start()
     while True:
         time.sleep(180)
-        living_processes = [1 for p in processes if p.is_alive()]
+        living_processes = [p for p in processes if p.is_alive()]
         if len(living_processes) < len(funcs):
             for p in living_processes:
                 p.terminate()
-            print("Oops: Some processes died")
+            # print("Oops: Some processes died")
 
 # schedule_tasks()
