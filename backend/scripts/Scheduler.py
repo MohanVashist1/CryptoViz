eef45e2fe942f214fc0a2bc14b422a5d0ca9981b
Ubuntu <cryptoViz@migOS.i2srdxtdb4nulcx1fq5f350dhg.bx.internal.cloudapp.net>
2020-03-14 17:45:34 +0000

    Added crypto_data scheduling to new pool multiprocessing.

--- backend/scripts/Scheduler.py
+++ backend/scripts/Scheduler.py
@@ -12,59 +12,50 @@ def global_binance_wrapper():
 
 def retrieve_top_gainers_daily():
     while True:
-        print("here")
         dh.retrieve_top_gainers_daily()
-        time.sleep(6)
+        time.sleep(3600)
 
 def retrieve_top_losers_daily():
     while True:
-        print("here")
         dh.retrieve_top_losers_daily()
-        time.sleep(6)
+        time.sleep(3600)
 
 def retrieve_top_losers_hourly():
     while True:
-        print("here")
         dh.retrieve_top_losers_hourly()
-        time.sleep(6)
+        time.sleep(3600)
 
 def retrieve_top_gainers_hourly():
     while True:
-        print("here")
         dh.retrieve_top_gainers_hourly()
-        time.sleep(6)
+        time.sleep(3600)
 
 def crypto_data_minute():
-    # bw = dh.BinanceWrapper()
     while True:
         bw.getAllCryptoDataBinance("1m", True)
         time.sleep(60)
 
 def crypto_data_five_minutes():
-    # bw = dh.BinanceWrapper()
     while True:
         bw.getAllCryptoDataBinance("5m", True)
         time.sleep(300)
 
 def crypto_data_hour():
-    # bw = dh.BinanceWrapper()
     while True:
         bw.getAllCryptoDataBinance("1h", True)
         time.sleep(3600)
 
 def crypto_data_day():
-    # bw = dh.BinanceWrapper()
     while True:
         bw.getAllCryptoDataBinance("1d", True)
         time.sleep(86400)
 
 def crypto_data_month():
-    # bw = dh.BinanceWrapper()
     while True:
         bw.getAllCryptoDataBinance("1M", True)
         time.sleep(2592000)
 
-def test(f_name):
+def run(f_name):
     if f_name == 'top_gainers_daily':
         retrieve_top_gainers_daily()
     elif f_name == 'top_losers_daily':
@@ -85,17 +76,11 @@ def test(f_name):
         crypto_data_month()
 
 def schedule_tasks():
-    #processes = []
-    #funcs = [retrieve_top_gainers_daily, retrieve_top_losers_daily, retrieve_top_gainers_hourly, retrieve_top_losers_hourly, crypto_data_minute, crypto_data_five_minutes, crypto_data_hour, crypto_data_day, crypto_data_month]
     funcs = ['top_gainers_daily', 'top_losers_daily', 'top_gainers_hourly', 'top_losers_hourly', 'data_minute', 'data_five_minutes', 'data_hour', 'data_day', 'data_month']
-    with mp.Pool(processes=mp.cpu_count(), initializer=global_binance_wrapper) as pool:
-        pool.map(test, funcs)
+    with mp.Pool(processes=len(funcs), initializer=global_binance_wrapper) as pool:
+        pool.map(run, funcs)
         pool.close()
         pool.join()
-    # for func in funcs:
-    #     process = mp.Process(target=func)
-    #     processes.append(process)
-    #     process.start()
     # while True:
     #     time.sleep(180)
     #     living_processes = [p for p in processes if p.is_alive()]
@@ -104,4 +89,4 @@ def schedule_tasks():
     #             p.terminate()
             # print("Oops: Some processes died")
 
-schedule_tasks()
+#schedule_tasks()
