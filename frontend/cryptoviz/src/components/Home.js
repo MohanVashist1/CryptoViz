953470ed001a6ef6ba7aff69ff12f5e344fa36a5
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-03-18 14:08:42 -0400

    Removed debug statements from gainers/losers frontend.

--- frontend/cryptoviz/src/components/Home.js
+++ frontend/cryptoviz/src/components/Home.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 // import Td from './Td'
 
-function Home({ match }) {
+function Home() {
   const [losersTimeInterval, setLosersTimeInterval] = useState("1");
   const [gainersTimeInterval, setGainersTimeInterval] = useState("1");
   const [losers, setLosers] = useState({});
@@ -23,7 +23,6 @@ function Home({ match }) {
   }, [losersTimeInterval]);
 
   const fetchLosers = async () => {
-    console.log(losersTimeInterval);
     const func = await fetch(
       `http://127.0.0.1:8000/api/losers/?time=${losersTimeInterval}`
     );
@@ -36,7 +35,6 @@ function Home({ match }) {
       `http://127.0.0.1:8000/api/gainers/?time=${gainersTimeInterval}`
     );
     const gainers = await func.json();
-    console.log(gainers)
     setGainers(gainers.gainers);
   };
 
@@ -46,45 +44,34 @@ function Home({ match }) {
   };
 
   const createTable = data => {
-    let table = [];
+    let rows = [];
     let rowClass = "table-primary";
-    // Outer loop to create parent
+    let cells = [];
     for (let i = 0; i < data.length; i++) {
-      let children = [];
-      //Inner loop to create children
-      // children.push(<th scope="row">Active</th>);
-      children.push(<td key={i + 1}>{data[i].rank}</td>);
-      children.push(<td key={i + 2}>{data[i].symbol}</td>);
-      children.push(<td key={i + 3}>${data[i].market_cap}</td>);
-      children.push(<td key={i + 4}>${data[i].price}</td>);
-      children.push(<td key={i + 5}>${data[i].volume}</td>);
-      //Create the parent and add the children
-      table.push(<tr key={i + 6} className={rowClass}>{children}</tr>);
+      cells.push(<td key={i + 1}>{data[i].rank}</td>);
+      cells.push(<td key={i + 2}>{data[i].symbol}</td>);
+      cells.push(<td key={i + 3}>${data[i].market_cap}</td>);
+      cells.push(<td key={i + 4}>${data[i].price}</td>);
+      cells.push(<td key={i + 5}>${data[i].volume}</td>);
+      rows.push(<tr key={i + 6} className={rowClass}>{cells}</tr>);
       rowClass = (rowClass == "table-primary") ? "table-secondary" : "table-primary";
+      cells = [];
     }
-    return table;
+    return rows;
   };
 
   return (
     <div>
       <div style={{ textAlign: "center", marginTop: "4em" }}>
-        <h1>
-          Top 10 Gainers ({timeMapping[gainersTimeInterval]})
-        </h1>
+        <h1>Top 10 Gainers ({timeMapping[gainersTimeInterval]})</h1>
         <div style={{ marginTop: "2em" }}>
           <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <button type="button" className="btn btn-primary">
-              Select Time Interval
-            </button>
+            <button type="button" className="btn btn-primary">Select Time Interval</button>
             <div className="btn-group" role="group">
               <button id="btnGroupDrop1" type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
               <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                <a className="dropdown-item" onClick={() => {setGainersTimeInterval("1");}}>
-                  {timeMapping["1"]}
-                </a>
-                <a className="dropdown-item" onClick={() => {setGainersTimeInterval("24");}}>
-                  {timeMapping["24"]}
-                </a>
+                <a className="dropdown-item" onClick={() => {setGainersTimeInterval("1");}}>{timeMapping["1"]}</a>
+                <a className="dropdown-item" onClick={() => {setGainersTimeInterval("24");}}>{timeMapping["24"]}</a>
               </div>
             </div>
           </div>
@@ -100,30 +87,20 @@ function Home({ match }) {
                 <th scope="col">Volume</th>
               </tr>
             </thead>
-            <tbody>
-              {createTable(gainers)}
-            </tbody>
+            <tbody>{createTable(gainers)}</tbody>
           </table> 
         </div>
       </div>
       <div style={{ textAlign: "center"}}>
-        <h1>
-          Top 10 Losers ({timeMapping[losersTimeInterval]})
-        </h1>
+        <h1>Top 10 Losers ({timeMapping[losersTimeInterval]})</h1>
         <div style={{ marginTop: "2em" }}>
           <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <button type="button" className="btn btn-primary">
-              Select Time Interval
-            </button>
+            <button type="button" className="btn btn-primary">Select Time Interval</button>
             <div className="btn-group" role="group">
               <button id="btnGroupDrop1" type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
               <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                <a className="dropdown-item" onClick={() => {setLosersTimeInterval("1");}}>
-                  {timeMapping["1"]}
-                </a>
-                <a className="dropdown-item" onClick={() => {setLosersTimeInterval("24");}}>
-                  {timeMapping["24"]}
-                </a>
+                <a className="dropdown-item" onClick={() => {setLosersTimeInterval("1");}}>{timeMapping["1"]}</a>
+                <a className="dropdown-item" onClick={() => {setLosersTimeInterval("24");}}>{timeMapping["24"]}</a>
               </div>
             </div>
           </div>
@@ -139,9 +116,7 @@ function Home({ match }) {
                 <th scope="col">Volume</th>
               </tr>
             </thead>
-            <tbody>
-              {createTable(losers)}
-            </tbody>
+            <tbody>{createTable(losers)}</tbody>
           </table> 
         </div>
       </div>
