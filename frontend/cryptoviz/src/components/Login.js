18585896955ed437d883360354a126ca5eed83c7
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-03-20 18:12:54 -0400

    Signup works from frontend.

--- frontend/cryptoviz/src/components/Login.js
+++ frontend/cryptoviz/src/components/Login.js
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+
+    const [errorMessage, setErrorMessage] = useState({});
+    const [email, setEmail] = useState({});
+    const [password, setPassword] = useState({});
+
+    const signUp = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 'email': email, 'password': password, 'watchlist': []})
+        };
+        fetchCommon('http://127.0.0.1:8000/api/users/register/', requestOptions);
+    };
+
+    const login = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: "email="+email+"&password="+password
+        };
+        fetchCommon('http://127.0.0.1:8000/api/users/login/cookie/', requestOptions);
+    };
+
+    const fetchCommon = (url, requestOptions) => {
+        fetch(url, requestOptions)
+          .then(async response => {
+            console.log(requestOptions);
+            const data = await response.json();
+            if (!response.ok) {
+              const error = (data && data.message) || response.status;
+              return Promise.reject(error);
+            }
+            console.log("done!")
+            // setLosers(data.losers);
+          })
+          .catch(error => {
+            setErrorMessage(error);
+            console.log(error);
+            console.error("There was an error!", error);
+        });
+    }
+
     return (
         <form style={{ width: "50%", margin: "auto", marginTop: "20vh" }}>
             <fieldset>
                 <legend style={{ textAlign: "center" }}><h2>Login / Sign Up</h2></legend>
                 <div className="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <label htmlFor="InputEmail">Email address</label>
+                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email" onChange={e => setEmail(e.target.value)}/>
                 </div>
                 <div className="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <label htmlFor="InputPassword">Password</label>
+                    <input type="password" className="form-control" id="InputPassword" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
                 </div>
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around"}}>
-                    <button type="submit" className="btn btn-primary">Log In</button>
-                    <button type="submit" className="btn btn-primary">Sign Up</button>
+                    <button type="button" className="btn btn-primary" onClick={login}>Log In</button>
+                    <button type="button" className="btn btn-primary" onClick={signUp}>Sign Up</button>
                 </div>
             </fieldset>
         </form>
