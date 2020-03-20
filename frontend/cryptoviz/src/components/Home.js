7408b5a75dbf22d69f11565e534309e171c203dc
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-03-19 21:32:20 -0400

    Changed key for Home.js

--- frontend/cryptoviz/src/components/Home.js
+++ frontend/cryptoviz/src/components/Home.js
@@ -46,14 +46,16 @@ function Home() {
     let rows = [];
     let rowClass = "table-primary";
     let cells = [];
+    let count = 0;
     for (let i = 0; i < data.length; i++) {
-      cells.push(<td key={i + 1}><Link to={`/crypto/${data[i].symbol}USDT/`}>{data[i].rank}</Link></td>);
-      cells.push(<td key={i + 2}><Link to={`/crypto/${data[i].symbol}USDT/`}>{data[i].symbol}</Link></td>);
-      cells.push(<td key={i + 3}><Link to={`/crypto/${data[i].symbol}USDT/`}>${data[i].market_cap}</Link></td>);
-      cells.push(<td key={i + 4}><Link to={`/crypto/${data[i].symbol}USDT/`}>${data[i].price}</Link></td>);
-      cells.push(<td key={i + 5}><Link to={`/crypto/${data[i].symbol}USDT/`}>${data[i].volume}</Link></td>);
-      rows.push(<tr key={i + 6} className={rowClass}>{cells}</tr>);
+      cells.push(<td key={count}><Link to={`/crypto/${data[i].symbol}USDT/`}>{data[i].rank}</Link></td>);
+      cells.push(<td key={count + 1}><Link to={`/crypto/${data[i].symbol}USDT/`}>{data[i].symbol}</Link></td>);
+      cells.push(<td key={count + 2}><Link to={`/crypto/${data[i].symbol}USDT/`}>${data[i].market_cap}</Link></td>);
+      cells.push(<td key={count + 3}><Link to={`/crypto/${data[i].symbol}USDT/`}>${data[i].price}</Link></td>);
+      cells.push(<td key={count + 4}><Link to={`/crypto/${data[i].symbol}USDT/`}>${data[i].volume}</Link></td>);
+      rows.push(<tr key={count + 5} className={rowClass}>{cells}</tr>);
       rowClass = (rowClass == "table-primary") ? "table-secondary" : "table-primary";
+      count += 6
       cells = [];
     }
     return rows;
