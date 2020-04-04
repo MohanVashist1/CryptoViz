c44acae2015578164e1e21742fd839b5c247cd7a
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-04-03 20:40:07 -0400

    Id name change.

--- frontend/cryptoviz/src/js/components/Navbar.js
+++ frontend/cryptoviz/src/js/components/Navbar.js
@@ -87,15 +87,15 @@ function Navbar() {
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarColor02"
-          aria-controls="navbarColor02"
+          data-target="#navbarColor01"
+          aria-controls="navbarColor01"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarColor02">
+        <div className="collapse navbar-collapse" id="navbarColor01">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
               <NavLink to="/" className="nav-link" activeClassName="active">
