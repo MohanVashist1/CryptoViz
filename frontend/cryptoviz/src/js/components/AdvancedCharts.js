367ce94365f27b2860cc8d717ecc77d2b84b278a
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-04-12 15:39:17 -0400

    Fixed advancedcharts navbar bug.

--- frontend/cryptoviz/src/js/components/AdvancedCharts.js
+++ frontend/cryptoviz/src/js/components/AdvancedCharts.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { TVChartContainer } from "../../components/TVChartContainer/index";
+import Loader from "react-loader-spinner";
+import Cookie from 'js-cookie';
 
 function AdvancedLandingPage({ match }) {
   return (
-    <div>
-      <Navbar />
-      <TVChartContainer tickerId={match.params.ticker}></TVChartContainer>
-    </div>
+    <>
+    {(Cookie.get('isLoggedIn') && Cookie.get('isLoggedIn').toLocaleLowerCase() === 'true' && authState.isAuthenticated) ||
+      ((!Cookie.get('isLoggedIn') || Cookie.get('isLoggedIn').toLocaleLowerCase() === 'false') && !authState.isAuthenticated) ?
+      <div>
+        <Navbar />
+        <TVChartContainer tickerId={match.params.ticker}></TVChartContainer>
+      </div> :
+      <div style={{ textAlign: "center", marginTop: "20em" }}>
+        <Loader type="ThreeDots" color="#2BAD60" />
+      </div>}
+    </>
   );
 }
 
