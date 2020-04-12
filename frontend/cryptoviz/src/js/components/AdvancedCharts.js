308719ff65e5dfe3704f3a4fab75f85bb3bb4758
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2020-04-12 15:53:12 -0400

    Fixed import in AdvancedCharts.

--- frontend/cryptoviz/src/js/components/AdvancedCharts.js
+++ frontend/cryptoviz/src/js/components/AdvancedCharts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import { AuthContext } from "./App";
 import { TVChartContainer } from "../../components/TVChartContainer/index";
