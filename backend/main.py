d406055858fd2b5cb4bdbcfd92bf6fe3281ec5e4
Mohan Vashist <mohan.l.vashist@gmail.com>
2020-04-12 21:14:56 -0400

    bug fix

--- backend/main.py
+++ backend/main.py
@@ -119,9 +119,11 @@ async def cookie_set(request: Request, call_next):
             cookie_arr = cookie.split('=')
             if cookie_arr[0] == 'user_auth':
                 if cookie_arr[1].split(';')[0] == '""':
-                    response.set_cookie("isLoggedIn", "false", secure=True, httponly=False, max_age=3600 * 24)
+                    response.set_cookie(
+                        "isLoggedIn", False, secure=True, httponly=False, max_age=3600 * 24)
                 else:
-                    response.set_cookie("isLoggedIn", "true", secure=True, httponly=False, max_age=3600 * 24)
+                    response.set_cookie(
+                        "isLoggedIn", True, secure=True, httponly=False, max_age=3600 * 24)
                 break
     for idx, header in enumerate(response.raw_headers):
         if header[0].decode("utf-8") == "set-cookie":
